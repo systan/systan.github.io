f3ccfd4707ddedc2e94daca61be4269a3f46c8b9
systan <mel5.ville15+sys@gmail.com>
2016-09-02 00:34:39 +0900

    更新

--- さわやか新店舗　藤枝Biviに｜静岡新聞アットエス_files/article.js
+++
@@ -1,9 +0,0 @@
-// 特集エリア用ファイルインクルード
-function writeFeatured(){
-	$.ajax({
-		url: "/ats-static/include/news/detail_featured.html",
-		success: function(html){
-			$('.divFeatured').append(html);
-		}
-	});
-}
